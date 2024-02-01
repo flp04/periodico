bf60fc4744521f19f3d03bdff58dc425185378e2
Filipe Husak <69702727+flp04@users.noreply.github.com>
2024-02-01 17:56:13 -0300

    corrigido responsividade do logo

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@ import HelloWorld from './components/HelloWorld.vue'
           <span></span>
         </div>
 
-        <img alt="Vue logo" style="width: 150px;" src="@/assets/logocabecera-1.png" />
+        <img alt="Vue logo" @click="irParaHome()" style="cursor: pointer; width: 150px;" src="@/assets/logocabecera-1.png" />
         <svg style="color: white;" xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/></svg>
       </div>
       <ul class="nav-list">
